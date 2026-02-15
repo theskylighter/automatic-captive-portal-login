2abc8aecf1c3cf0c0ec3d5c7d5925cd1174dd084
Shivam Singh <2023uai1819@mnit.ac.in>
2026-02-16 01:16:44 +0530

    added linux bash script

---
+++ login.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Activate Python environment if needed
+# source /path/to/venv/bin/activate
+
+
+# Run the Python script
+/usr/bin/python3 /home/username/scripts/login.py
+
+# UPDATE USERNAME AND PATH AS NEEDED
